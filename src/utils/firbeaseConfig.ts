302867de82e15d66088975a8ff0666f68702fb85
Techchauhan <rishabkumarchauhan@gmail.com>
2024-12-25 19:37:36 +0530

    firebase config

--- src/utils/firbeaseConfig.ts
+++ src/utils/firbeaseConfig.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC4X191jlI5WONorU4OkfFa844XzQYkFnk",
+  authDomain: "ecommerce-with-pulsezest.firebaseapp.com",
+  projectId: "ecommerce-with-pulsezest",
+  storageBucket: "ecommerce-with-pulsezest.firebasestorage.app",
+  messagingSenderId: "163595114954",
+  appId: "1:163595114954:web:4bbe97ef15ea8036949733",
+  measurementId: "G-HRHZMT8TC8"
 };
 
 // Initialize Firebase
